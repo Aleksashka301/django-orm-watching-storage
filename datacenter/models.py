7f41b1ea8cbbd22a3f2d4beca81a22b02a31e0bf
Aleksashka301 <aleksashka301@gmail.com>
2024-12-09 18:59:31 +0500

    Update models.py

--- datacenter/models.py
+++ datacenter/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from django.utils.timezone import localtime
 
 
 class Passcard(models.Model):
@@ -29,25 +28,3 @@ class Visit(models.Model):
                 if self.leaved_at else 'not leaved'
             )
         )
-
-
-def get_duration(visit):
-    visit_in = localtime(visit.entered_at)
-    if not visit.leaved_at:
-        visit_out = localtime()
-    else:
-        visit_out = localtime(visit.leaved_at)
-
-    return visit_out - visit_in
-
-def format_duration(visit):
-    total_time = visit.total_seconds()
-    total_hour = 0
-
-    if total_time >= 3600:
-        total_hour = int(total_time / 3600)
-        total_time -= total_hour * 3600
-
-    total_min = total_time / 60
-    return f'{total_hour}ч {int(total_min)}мин'
-
