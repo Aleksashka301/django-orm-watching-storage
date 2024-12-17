c45c7bf109e34b7655d61b021de6b4beaef5eff8
Aleksashka301 <aleksashka301@gmail.com>
2024-12-17 17:16:54 +0500

    Update settings.py

--- project/settings.py
+++ project/settings.py
@@ -1,21 +1,37 @@
 import os
+from environs import Env
+
+
+env = Env()
+env.read_env()
+
+engine = env('ENGINE')
+host = env('HOST')
+port = env('PORT')
+name = env('NAME')
+guard = env('GUARD')
+password_db = env('PASSWORD_DB')
+
+secret_key = env('SECRET_KEY')
+debug = env.bool('DEBUG', default=False)
+
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'HOST': 'checkpoint.devman.org',
-        'PORT': '5434',
-        'NAME': 'checkpoint',
-        'USER': 'guard',
-        'PASSWORD': 'osim5',
+        'ENGINE': engine,
+        'HOST': host,
+        'PORT': port,
+        'NAME': name,
+        'USER': guard,
+        'PASSWORD': password_db,
     }
 }
 
 INSTALLED_APPS = ['datacenter']
 
-SECRET_KEY = 'REPLACE_ME'
+SECRET_KEY = secret_key
 
-DEBUG = True
+DEBUG = debug
 
 ROOT_URLCONF = 'project.urls'
 
