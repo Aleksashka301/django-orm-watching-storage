98844cc82245ed4e128e33845472e811351d8e10
Aleksashka301 <aleksashka301@gmail.com>
2024-12-20 18:22:50 +0500

    Update passcard_info_view.py

--- datacenter/passcard_info_view.py
+++ datacenter/passcard_info_view.py
@@ -2,6 +2,7 @@ from datacenter.models import Passcard
 from datacenter.models import Visit
 from datacenter.extra_functionality import get_duration
 from datacenter.extra_functionality import format_duration
+from datacenter.extra_functionality import calculation_time
 from django.shortcuts import render
 from django.shortcuts import get_object_or_404
 
@@ -17,7 +18,7 @@ def passcard_info_view(request, passcode):
         visit_info = {
             'entered_at': visit.entered_at,
             'duration': format_duration(total_time),
-            'is_strange': total_time.total_seconds() >= 3600,   # сравниваем затраченное время на визит 3 600 сек. (1час)
+            'is_strange': total_time.total_seconds() >= calculation_time(60),
         }
         this_passcard_visits.append(visit_info)
 
