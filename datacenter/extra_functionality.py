3728755f3e03102ddabfe7057ee34eb8d1cf9a2a
Aleksashka301 <aleksashka301@gmail.com>
2024-12-22 17:54:22 +0500

    Названия переменных и функции
    Изменены названия переменных : SEC_HOUR, MIN_HOUR, SEC_MINUTE на более понятные SECONDS_HOUR, MINUTES_HOUR, SECONDS_MINUTE. Изменено название функции calculation_time на suspicious_visit_time.

--- datacenter/extra_functionality.py
+++ datacenter/extra_functionality.py
@@ -1,6 +1,11 @@
 from django.utils.timezone import localtime
 
 
+SECONDS_HOUR = 3600
+MINUTES_HOUR = 60
+SECONDS_MINUTE = 60
+
+
 def get_duration(visit):
     visit_in = localtime(visit.entered_at)
     if not visit.leaved_at:
@@ -12,20 +17,16 @@ def get_duration(visit):
 
 
 def format_duration(visit):
-    SEC_HOUR = 3600
-    MIN_HOUR = 60
-
     total_time = visit.total_seconds()
     total_hour = 0
 
-    if total_time >= SEC_HOUR:
-        total_hour = int(total_time / SEC_HOUR)
-        total_time -= total_hour * SEC_HOUR
+    if total_time >= SECONDS_HOUR:
+        total_hour = int(total_time / SECONDS_HOUR)
+        total_time -= total_hour * SECONDS_HOUR
 
-    total_min = total_time / MIN_HOUR
+    total_min = total_time / MINUTES_HOUR
     return f'{total_hour}ч {int(total_min)}мин'
 
 
-def calculation_time(time_minute=60):
-    SEC_MINUTE = 60
-    return time_minute * SEC_MINUTE
+def suspicious_visit_time(time_minute=60):
+    return time_minute * SECONDS_MINUTE
