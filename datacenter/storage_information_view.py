4d700aa27b32f7aecc017905c8d27997af48a578
Aleksashka301 <aleksashka301@gmail.com>
2024-12-22 17:54:51 +0500

    Update storage_information_view.py

--- datacenter/storage_information_view.py
+++ datacenter/storage_information_view.py
@@ -2,7 +2,7 @@ from datacenter.models import Passcard
 from datacenter.models import Visit
 from datacenter.extra_functionality import get_duration
 from datacenter.extra_functionality import format_duration
-from datacenter.extra_functionality import calculation_time
+from datacenter.extra_functionality import suspicious_visit_time
 from django.shortcuts import render
 from django.utils.timezone import localtime
 
@@ -16,7 +16,7 @@ def storage_information_view(request):
             'who_entered': visit.passcard,
             'entered_at': localtime(visit.entered_at),
             'duration': format_duration(get_duration(visit)),
-            'is_strange': get_duration(visit).total_seconds() >= calculation_time(60),
+            'is_strange': get_duration(visit).total_seconds() >= suspicious_visit_time(60),
         }
         non_closed_visits.append(visit_info)
 
